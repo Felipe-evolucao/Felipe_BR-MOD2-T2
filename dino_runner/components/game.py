f5dd38b6bca523803253b165689068ab557d37d7
Felipe-evolucao <Felipe.evolucao.dev@gmail.com>
2022-09-27 20:58:01 -0300

    Obstaculos: Cacto pequeno

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,8 +1,8 @@
 import pygame
 
 from dino_runner.components.dinosaur import Dinosaur
-
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 
 class Game:
@@ -12,11 +12,12 @@ class Game:
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.player = Dinosaur()
         self.playing = False
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -26,21 +27,21 @@ class Game:
             self.update()
             self.draw()
         pygame.quit()
-
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
-
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill("#FFFFFF") #Também aceita código hexadecimal "#FFFFFF"
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
