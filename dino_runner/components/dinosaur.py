14dbc5ce8f963c0a37c3200279fd33a57736acee
Felipe-evolucao <Felipe.evolucao.dev@gmail.com>
2022-09-26 22:28:38 -0300

    def Duck()
    Inclusão da função Duck e vinculada a tecla K_Down

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,14 +1,14 @@
 from ast import If
 import pygame
-from dino_runner.utils.constants import JUMPING, RUNNING
+from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
+
 X_POS = 80
 Y_POS = 310
-
 
 class Dinosaur:
 
     JUMP_VEL = 8.5
-    
+
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -17,6 +17,7 @@ class Dinosaur:
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
 
     def update(self, user_input):
@@ -24,6 +25,8 @@ class Dinosaur:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -31,6 +34,10 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+
+        if user_input[pygame.K_DOWN]:
+            self.dino_run = False
+            self.dino_duck = True
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -52,6 +59,14 @@ class Dinosaur:
             self.dino_rect.y = Y_POS
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = 342
+        self.step_index += 1
+        
 
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
